5b262f57e8a97e604ab8f4cda74c14e1af3feeec
Quang-Ngu Truong <zquangu112z@gmail.com>
2019-09-07 23:35:22 +0700

    feat: add ListLink

--- src/components/container.js
+++ src/components/container.js
@@ -1,13 +1,25 @@
 import React from "react"
 import containerStyles from "./container.module.css"
 import { Link } from "gatsby"
-
+const ListLink = props => (
+  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+    <Link to={props.to}>{props.children}</Link>
+  </li>
+)
 export default ({ children }) => (
   <div className={containerStyles.container}>
-    <h3>Autoblog</h3>
-    <Link to="/">Home</Link>
-    <Link to="/about">About</Link>
-    <Link to="/about-css-modules">About-CSS</Link>
+    <Link to="/" className={containerStyles.siteTitle}>
+      <h3 style={{ display: `inline` }}>Autoblog</h3>
+    </Link>
+
+    <ul style={{ listStyle: `none`, float: `right` }}>
+      <ListLink to="/">Home</ListLink>
+      <ListLink to="/about">About</ListLink>
+      <ListLink to="/about-css-modules">About-CSS</ListLink>
+    </ul>
     {children}
+    <footer>
+      (c) Quang-Ngu Truong 2019
+    </footer>
   </div>
 )
